db64cc0faa36df028f259c981b5b2bd6c62e4e6e
chitai <2551050199tai@ou.edu.vn>
2026-08-18 21:27:47 +0700

    formatted

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,19 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const hamburgerBtn = document.querySelector('.hamburger-btn');
-  const navbarMenu = document.querySelector('.navbar-menu');
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburgerBtn = document.querySelector(".hamburger-btn");
+  const navbarMenu = document.querySelector(".navbar-menu");
 
   if (hamburgerBtn && navbarMenu) {
-    hamburgerBtn.addEventListener('click', (e) => {
+    hamburgerBtn.addEventListener("click", (e) => {
       e.stopPropagation();
-      hamburgerBtn.classList.toggle('is-active');
-      navbarMenu.classList.toggle('is-active');
+      hamburgerBtn.classList.toggle("is-active");
+      navbarMenu.classList.toggle("is-active");
     });
 
-    // Tự động đóng menu khi bấm ra ngoài
-    document.addEventListener('click', (e) => {
+    document.addEventListener("click", (e) => {
       if (!navbarMenu.contains(e.target) && !hamburgerBtn.contains(e.target)) {
-        hamburgerBtn.classList.remove('is-active');
-        navbarMenu.classList.remove('is-active');
+        hamburgerBtn.classList.remove("is-active");
+        navbarMenu.classList.remove("is-active");
       }
     });
   }
