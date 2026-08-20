dd53063f7fdaebce9a1e89e2528243aa5b7a06a7
Kim Quang Tu <taitulynhom14@gmail.com>
2026-08-20 23:26:07 +0700

    feat(vite.config): add rollup input configuration for multiple HTML pages

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,41 @@ export default defineConfig({
       partialDirectory: resolve(__dirname, "partials"),
     }),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        // Trang chủ & các trang ở thư mục gốc
+        main: resolve(__dirname, "index.html"),
+        favorites: resolve(__dirname, "favorites.html"),
+
+        // Thư mục flight-flow
+        findFlight: resolve(__dirname, "flight-flow/find-flight.html"),
+        flightBookingDetailTicket: resolve(__dirname, "flight-flow/flight-booking-detail-ticket.html"),
+        flightBookingDetailVisa: resolve(__dirname, "flight-flow/flight-booking-detail-visa.html"),
+        flightBookingDetail: resolve(__dirname, "flight-flow/flight-booking-detail.html"),
+        flightDetailPage: resolve(__dirname, "flight-flow/flight-detail-page.html"),
+        flightListing: resolve(__dirname, "flight-flow/flight-listing.html"),
+
+        // Thư mục hotel-flow
+        findStays: resolve(__dirname, "hotel-flow/find-stays.html"),
+        hotelBookingDetailTicket: resolve(__dirname, "hotel-flow/hotel-booking-detail-ticket.html"),
+        hotelBookingDetailVisa: resolve(__dirname, "hotel-flow/hotel-booking-detail-visa.html"),
+        hotelBookingDetail: resolve(__dirname, "hotel-flow/hotel-booking-detail.html"),
+        hotelDetailPage: resolve(__dirname, "hotel-flow/hotel-detail-page.html"),
+        hotelListing: resolve(__dirname, "hotel-flow/hotel-listing.html"),
+
+        // Thư mục login-signup
+        accountHistoryFlights: resolve(__dirname, "login-signup/account_history-flights.html"),
+        accountHistoryStays: resolve(__dirname, "login-signup/account_history-stays.html"),
+        accountPaymentMethods: resolve(__dirname, "login-signup/account_payment-methods.html"),
+        account: resolve(__dirname, "login-signup/account.html"),
+        forgotpassSetNew: resolve(__dirname, "login-signup/forgotpass_set-new.html"),
+        forgotpassVerify: resolve(__dirname, "login-signup/forgotpass_verify.html"),
+        forgotpass: resolve(__dirname, "login-signup/forgotpass.html"),
+        login: resolve(__dirname, "login-signup/login.html"),
+        signupPaymentMethods: resolve(__dirname, "login-signup/signup_payment-methods.html"),
+        signup: resolve(__dirname, "login-signup/signup.html"),
+      },
+    },
+  },
 });
